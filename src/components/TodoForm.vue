a83dea8d934ad521a23842f5ca5105c0b58a5141
Mikiko Noro <miriks818@gmail.com>
2022-04-07 16:52:20 +0900

    Formを追加

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-container>
+      <b-input-group>
+        <b-form-input type="text" v-model="value" />
+        <b-input-group-append>
+          <b-button variant="info">送信</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  data() {
+    return {
+      value: '',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
